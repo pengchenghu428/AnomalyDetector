caba389c2779633e5f89f563a8e25df08728a2b1
pengcheng_hu <15651851181@163.com>
2019-12-23 22:02:58 +0800

    初始化仓库

---
+++ demo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：AnomalyDetector -> demo
+@IDE    ：PyCharm
+@Author ：pengchenghu
+@Date   ：2019/12/23 21:52
+@Desc   ：测试案例
+=================================================='''
+
+
+
